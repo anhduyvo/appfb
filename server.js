f209046b1c6cac83bd6a0b676702b60573024523
duyanh <duyanh2005@gmail.com>
2017-08-28 23:53:39 +0700

    init project

---
+++ server.js
@@ -0,0 +1,22 @@
+﻿const express = require('express');
+const http = require('http');
+const path = require("path");
+
+const server = express();
+//server.set('port', process.env.PORT || 3000);
+server.set('port', 3000);
+
+/**
+ * register API 
+ */
+server.use('/api', require('./routes/api'));
+
+/**
+ * register admin site collection
+ */
+server.use('/admin', express.static(path.join(__dirname, 'admin'), { index: 'default.html' }));
+
+const webpath = path.join(__dirname, 'client'); 
+server.use('/', express.static(webpath, { index: 'default.html' }));
+
+module.exports = server;
