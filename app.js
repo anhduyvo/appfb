5aedd1fc5cd8666f32cdc9ea5a9a2ebdf629c583
Anh <voduyanh1984@gmail.com>
2020-09-16 17:38:54 +0700

    - up version & fix logs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 ﻿var server = require('./server');
 var app = server.listen(server.get('port'), function () {    
-    console.log('eCompany is running on port: %s by nodejs version %s', app.address().port, process.version);
+    console.log('eShop is running on port: %s by nodejs version %s', app.address().port, process.version);
 });
 
 module.exports = app;
